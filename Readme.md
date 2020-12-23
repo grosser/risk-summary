62318b153ff754f7e51a10b4785286637a22bc65
Michael Grosser <michael@grosser.it>
2020-12-23 11:58:33 -0800

    POC

--- Readme.md
+++ Readme.md
@@ -1,9 +1,19 @@
-Collects Risk section from all merged PRs over a given commit range
+<!-- keep in sync with bundle exec ./bin/risk-summary --help -->
+Collects Risk section from all merged PRs over a given commit range.
+
+Your github token needs to be available as `GITHUB_TOKEN` env var or `git config github.token`.
 
 ```bash
 gem install risk-summary
 risk-summary v11..v20
 ```
+
+TODO
+====
+- support token via cli argument
+- support github enterprise with custom domain
+- support gitlab
+- support commit ranges with >250 commits [details](https://stackoverflow.com/questions/26925312/github-api-how-to-compare-2-commits)
 
 Author
 ======
--- lib/risk_summary.rb
+++ lib/risk_summary.rb
@@ -1,29 +1,121 @@
 # frozen_string_literal: true
 require "optparse"
+require "net/http"
+require "json"
 
 module RiskSummary
   VERSION = "0.0.0"
 
+  # copied from https://github.com/zendesk/samson/blob/master/app/models/changeset/pull_request.rb
+  # modified to use unsafe regex stripping
+  module RiskParser
+    class << self
+      def parse(text)
+        risks = parse_risks(text)
+        if risks.match?(/\A\s*-?\s*None\Z/i)
+          []
+        elsif risks.empty?
+          nil
+        else
+          risks
+        end
+      end
+
+      private
+
+      def parse_risks(body)
+        body_stripped = body.gsub(%r{</?[^>]+?>}, "") # not safe, but much simpler than pulling in nokogiri
+        section_content('Risks', body_stripped).to_s.rstrip.sub(/\A\s*\n/, "")
+      end
+
+      def section_content(section_title, text)
+        # ### Risks or Risks followed by === / ---
+        desired_header_regexp = "^(?:\\s*#+\\s*#{section_title}.*|\\s*#{section_title}.*\\n\\s*(?:-{2,}|={2,}))\\n"
+        content_regexp = '([\W\w]*?)' # capture all section content, including new lines, but not next header
+        next_header_regexp = '(?=^(?:\s*#+|.*\n\s*(?:-{2,}|={2,}\s*\n))|\z)'
+
+        text[/#{desired_header_regexp}#{content_regexp}#{next_header_regexp}/i, 1]
+      end
+    end
+  end
+
   class << self
     def cli(argv)
-      OptionParser.new do |opts|
-        opts.banner = <<~BANNER
-          Collects Risk section from all merged PRs over a given commit range
+      parse_cli_options! argv
+      token = fetch_token
+      puts risks(*argv, token)
+      0
+    end
+
+    private
+
+    def risks(repo, diff, token)
+      compare = http_get "/repos/#{repo}/compare/#{diff}", token
+
+      # TODO: parallel threads
+      pulls = compare.fetch(:commits).flat_map do |commit|
+        http_get "/repos/#{repo}/commits/#{commit.fetch(:sha)}/pulls", token
+      end.uniq
+
+      pulls.flat_map do |pull|
+        if risks = RiskParser.parse(pull.fetch(:body))
+          risks
+        else
+          ["- missing risks from [#{pull.fetch(:number)}](#{pull.fetch(:html_url)})"]
+        end
+      end
+    end
+
+    def parse_cli_options!(argv)
+      parser = OptionParser.new do |p|
+        p.banner = <<~BANNER
+          Collects Risk section from all merged PRs over a given commit range.
+          Your github token needs to be available as `GITHUB_TOKEN` env var or `git config github.token`.
 
           Usage:
-              risk-summary v4..v12
+              risk-summary zendesk/samson v3240...v3250
 
           Options:
         BANNER
-        opts.on("-h", "--help", "Show this.") do
-          puts opts
+        p.on("-h", "--help", "Show this.") do
+          puts p
           exit 0
         end
-        opts.on("-v", "--version", "Show Version") do
+        p.on("-v", "--version", "Show Version") do
           puts VERSION
           exit 0
         end
-      end.parse!(argv)
+      end
+      parser.parse!(argv)
+
+      if argv.size != 2
+        puts parser
+        exit 1
+      end
+    end
+
+    def fetch_token
+      ENV["GITHUB_TOKEN"] ||
+        token_from_gitconfig ||
+        raise("Unable to find github token in GITHUB_TOKEN env var or git config github.token")
+    end
+
+    def token_from_gitconfig
+      result = `git config github.token`.chomp
+      result if $?.success?
+    end
+
+    def http_get(path, token)
+      url = "https://api.github.com#{path}"
+      uri = URI(url)
+      req = Net::HTTP::Get.new(uri)
+      req["Authorization"] = "token #{token}"
+      req["Accept"] = "application/vnd.github.groot-preview+json" # for /pulls requests
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+      raise "Bad response from #{url}:\n#{res.code}\n#{res.body}" unless res.code == "200"
+      JSON.parse(res.body, symbolize_names: true)
     end
   end
 end
